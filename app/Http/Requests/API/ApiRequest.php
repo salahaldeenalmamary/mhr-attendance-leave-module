6dd5b666b146e9164ce2542a42a1bd527b1351b0
SalahaldeenAlmamari <salahalmamary684@gmail.com>
2025-06-10 22:52:20 +0300

    add Base request class for all API form requests.

---
+++ app/Http/Requests/API/ApiRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests\API;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use App\Http\Resources\API\ApiResponseResource;
+
+/**
+ * Base request class for all API form requests.
+ */
+abstract class ApiRequest extends FormRequest
+{
+   
+    abstract public function rules();
+
+   
+   
+
+   
+    protected function failedValidation(Validator $validator)
+    {
+        $response = ApiResponseResource::error(
+            $validator->errors(), 
+            'The given data was invalid. Please check the errors.'
+        )->response()->setStatusCode(422);
+
+        throw new HttpResponseException($response);
+    }
+}
